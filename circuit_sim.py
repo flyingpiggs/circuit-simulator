38b9264c6cf90bef7692b3fb597e89095f43b307
flyingpiggs <peterigb2@gmail.com>
2020-06-03 17:15:10 -0500

    Wrote skeleton code

--- circuit_sim.py
+++ circuit_sim.py
@@ -1,19 +1,31 @@
 class Node:
-    def __init__( self, _name, _inputs, _outputs, _type, _value ):
+    def __init__( self, _name, _inputs, _type, _value ):
         self.name = _name            #string
         self.inputs = _inputs        #list of strings that correspond to the input nodes names
         self.type = _type            #string
         self.value = _value          #string, although limited to one character
-        self.outputReady = False     
+        self.outputReady = False
+
+def MakeNodes( benchName ):
+    pass
+
+def GetTestVectors( fileName ):
+    pass
+
+def Simulate( circuit, testVectors ):
+    pass
 
 def main():
-    # Read-in circuit benchmark
-    # Create the circuit graph
+    # Read-in circuit benchmark and create circuit nodes
+    userInput = input()
+    circuit = MakeNodes( userInput )
     # Read-in test vectors
-    # Performance simulation/breath-first search through the circuit
-    # Most likely do other things depending on what the assignment calls for
+    userInput = input()
+    testVectors = GetTestVectors( userInput )
+    # Performance simulation/breadth-first search through the circuit
+    # Simultaneously do some other stuff depending on what the assignment calls for?
     # Ex: Calculate the critical path (longest delay path)
-    pass
+    Simulate( circuit, testVectors )
 
 if __name__ == "__main__":
     main()
