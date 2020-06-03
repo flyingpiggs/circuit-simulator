8ca425f3cb9b307c3c05226d9acaf41aeb7413eb
flyingpiggs <peterigb2@gmail.com>
2020-06-03 18:10:12 -0500

    Made PerformOp function

--- circuit_sim.py
+++ circuit_sim.py
@@ -6,8 +6,118 @@ class Node:
         self.value = _value          #string, although limited to one character
         self.outputReady = False
 
+    def PerformOp( self, nodes ):
+        if ( self.type == 'input' or self.type == 'output' ):
+            print("Error, type cannot be 'input' or 'output' for operations")
+            return False
+
+        op = self.type
+        if ( op == 'AND' ):
+            for inputName in self.inputs:
+                val = GetVal( inputName, nodes )
+                if ( val == '0' ):
+                    self.value = '0'
+                    return True
+                elif ( val == '1' ):
+                    continue
+                else:
+                    print("Error, unsupported value at node: " + inputName )
+                    return False
+            self.value = '1'
+        elif ( op == 'OR' ):
+            for inputName in self.inputs:
+                val = GetVal( inputName, nodes )
+                if ( val == '1' ):
+                    self.value = '1'
+                    return True
+                elif ( val == '0' ):
+                    continue
+                else:
+                    print("Error, unsupported value at node: " + inputName )
+                    return False
+            self.value = '0'
+        elif ( op == 'NOR' ):
+            for inputName in self.inputs:
+                val = GetVal( inputName, nodes )
+                if ( val == '1' ):
+                    self.value = '0'
+                    return True
+                elif ( val == '0' ):
+                    continue
+                else:
+                    print("Error, unsupported value at node: " + inputName )
+                    return False
+            self.value = '1'
+        elif ( op == 'NAND' ):
+            for inputName in self.inputs:
+                val = GetVal( inputName, nodes )
+                if ( val == '0' ):
+                    self.value = '1'
+                    return True
+                elif ( val == '1' ):
+                    continue
+                else:
+                    print("Error, unsupported value at node: " + inputName )
+                    return False
+            self.value = '0'
+        elif ( op == 'XOR' ):
+            onesCount = 0
+            for inputName in self.inputs:
+                val = GetVal( inputName, nodes )
+                if ( val == '1' ):
+                    onesCount += 1
+                elif ( val == '0' ):
+                    continue
+                else:
+                    print("Error, unsupported value at node: " + inputName )
+                    return False
+            if ( ( onesCount % 2 ) == 1 ):   #odd number of 1s
+                self.value = '1'
+            else:
+                self.value = '0'
+        elif ( op == 'XNOR' ):
+            onesCount = 0
+            for inputName in self.inputs:
+                val = GetVal( inputName, nodes )
+                if ( val == '1' ):
+                    onesCount += 1
+                elif ( val == '0' ):
+                    continue
+                else:
+                    print("Error, unsupported value at node: " + inputName )
+                    return False
+            if ( ( onesCount % 2 ) == 0 ):   #odd number of 1s
+                self.value = '1'
+            else:
+                self.value = '0'
+        elif ( op == 'NOT' ):
+            val = GetVal( inputs[0], nodes )
+            if ( val == '1' ):
+                val = '0'
+            elif ( val == '0' ):
+                val = '1'
+            else:
+                print("Error, unsupported value at node: " + inputName )
+                self.value = '?'
+                return False
+            self.value = val
+        elif ( op == 'BUFF' ):
+            self.value = GetVal( inputs[0], nodes )
+        else:
+            print("Error, unsupported operation at node: " + self.name )
+            return False
+        return True     #just to indicate success
+
 def MakeNodes( benchName ):
     pass
+
+# This function traverses the graph looking for a name match
+# Once a match is found, it returns the value
+def GetVal( name, nodes ):
+    for node in nodes:
+        if ( node.name == name ):
+            return node.value
+    return None   #This indicates an error since there should always be a match
 
 def GetTestVectors( fileName ):
     pass
